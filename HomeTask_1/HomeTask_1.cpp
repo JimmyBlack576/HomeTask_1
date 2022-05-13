6a29a14b4bf0c5415fff5dbca7b707c669ff1010
Jimmy Blackwood <Jimmy Blackwood@DESKTOP-AOM0ABL>
2022-05-13 23:49:26 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_1/HomeTask_1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    float distance, time, speed;
+    cout << "Введите расстояние (км): \n";
+    cin >> distance;
+    cout << "Введите время (мин): \n";
+    cin >> time;
+
+    cout <<"Вам нужно двигаться со скоростью " << distance / time <<"(км/м)";
+}
+
